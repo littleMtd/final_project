c6da91d5729dd49c423a5674aba24ce80a5fda81
littleMtd <B11423041@live.yuntech.edu.tw>
2025-12-30 23:58:24 +0800

    chore: update tooling and deploy configs

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,16 @@ export default defineConfig({
     }
   },
   server: {
-    port: parseInt(process.env.VITE_PORT || '3000')
+    port: parseInt(process.env.VITE_PORT || '3000'),
+    proxy: {
+      '/expense': 'http://localhost:8000',
+      '/income': 'http://localhost:8000',
+      '/purpose': 'http://localhost:8000',
+      '/ledger': 'http://localhost:8000',
+      '/report': 'http://localhost:8000',
+      '/insights': 'http://localhost:8000',
+      '/signup': 'http://localhost:8000',
+      '/signin': 'http://localhost:8000'
+    }
   }
 })
